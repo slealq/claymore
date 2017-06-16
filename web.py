0f760b5ee9150b8cf56c2d7410c9e3a20b1172c2
Stuart Leal Quesada <stuart.leal23@gmail.com>
2017-06-16 09:38:58 -0600

    Added a visible and close attribute to the available_curses, to choose whether or not to display the browser and to close the browser at the end.

--- web.py
+++ web.py
@@ -1,28 +1,44 @@
 from selenium import webdriver
-from pyvirtualdispaly import Display
+from xvfbwrapper import Xvfb
 from bs4 import BeautifulSoup
 
-def available_curses(carne, passw):
+def available_curses(carne, passw, visible=False, close=True):
     """
     Enters ematricula.ucr.ac.cr and then
     goes to the section that has the information
     about available careers.
 
-    This should also download the color, since
-    this is how in the page you now which curses
-    are available to you.
+    Example usage:
+
+    Launch a invisible browser and get the curses.
+    -> available_curses('b53777', 'mypassword')
+    
+    Launch a visible browser to see step by step what is done.
+    -> available_curses('b53777', 'mypassword', visible=True)
+
+    Launch a visible browser and don't close when finished.
+    -> available_curses('b53777', 'mypassword', visible=True, close=False)
     """
 
-    # Should use RoboBrowser or requests, but for now using Selenium
-    driver = webdriver.Firefox()
-    # Go to ematricula 
+    # Create and start a virtualdisplay
+    vdisplay = Xvfb()
+
+    # Launch the display if visible is false
+    if not visible:
+        vdisplay.start()
+    
+    # Open the browser and do the magic
+    driver = webdriver.Firefox() 
     driver.get('https://ematricula.ucr.ac.cr/ematricula/login.do')
-    # Search for the boxes by the name and the fill them
     carne_box = driver.find_element_by_name('carne')
     pass_box = driver.find_element_by_name('pin')
     carne_box.send_keys(carne)
     pass_box.send_keys(passw)
     
-    
+    # Quit the browser
+    if close:
+        driver.quit()
 
-
+    # If not visible, stop display
+    if not visible:
+        vdisplay.stop()
